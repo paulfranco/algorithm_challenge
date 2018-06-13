57340fec00f90e2d2a4b79016c9ded6ff981dd0e
Paul Franco <paulfranco@me.com>
2018-06-12 23:53:20 -0700

    adding biggieSize.js

---
+++ biggieSize.js
@@ -0,0 +1,15 @@
+// Biggie Size
+// Given an array. Write a function that changes all positive numbers in the array to the string "big". 
+
+var myArr = [-2,2,5,4,7,8,-22, -1];
+
+function big(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      arr[i] = "big";
+    }
+  }
+  return arr;
+}
+
+big(myArr);
