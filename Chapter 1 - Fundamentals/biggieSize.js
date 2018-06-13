c676413bae36b98977521305e3a519b63404a7bc
Paul Franco <paulfranco@me.com>
2018-06-13 00:02:33 -0700

    added chapter 1 folder

--- Chapter 1 - Fundamentals/biggieSize.js
+++ Chapter 1 - Fundamentals/biggieSize.js
@@ -2,6 +2,7 @@
 // Page 59
 // Biggie Size
 // Given an array. Write a function that changes all positive numbers in the array to the string "big". 
+// Video: https://youtu.be/RQt_4xAI_E8
 
 var myArr = [-2,2,5,4,7,8,-22, -1];
 
