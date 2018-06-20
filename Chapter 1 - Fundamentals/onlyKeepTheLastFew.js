96824d79f3bce6af9402adab124b7673cf25c13f
Paul Franco <paulfranco@me.com>
2018-06-19 23:19:24 -0700

    Adding another algorithm

---
+++ Chapter 1 - Fundamentals/onlyKeepTheLastFew.js
@@ -0,0 +1,4 @@
+// Only Keep the last few
+// Given an array and number X, remove all exceprt the last X elements, and return the array (changed and shorter) 
+// Given ([2,4,6,8,10], 3), change the array to [6,8,10] and return it.
+
